e216bd03baf67eab2abcdcd2c2dad5335e30c715
Sudish Shrestha <sudishrestha@localhost.localdomain>
2020-03-10 14:34:06 +0530

    Added more fields

--- custom_product_model.py
+++ custom_product_model.py
@@ -4,4 +4,8 @@ from datetime import datetime, timedelta
 from odoo.exceptions import ValidationError
 class ProductCustomizeTask(models.Model):
 	_inherit = 'product.template'
-	donation = fields.Boolean('Donation')
+	donation = fields.Boolean('Donation', store=True)
+	mohpEM = fields.Boolean('MOHP Essesntial Medicine')
+	govtSupply = fields.Boolean('Government Supply')
+	orphanDrug = fields.Boolean('Orphan Drug')
+	dentalItem = fields.Boolean('Dental Item')
